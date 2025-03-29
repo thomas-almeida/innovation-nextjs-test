73aea5bfd5efa5ccd58ff0cc8061459023e6bdee
thomas-almeida <contato.thomasalmeidard@gmail.com>
2025-03-29 14:56:02 -0300

    Login Button

---
+++ client/components/ui/LoginButton.jsx
@@ -0,0 +1,13 @@
+export default function LoginButton({
+
+}) {
+  return (
+    <div className="flex justify-center items-center mt-10">
+      <button
+        className="bg-white rounded-full p-4 text-black font-semibold w-[45%] cursor-pointer"
+      >
+        Login
+      </button>
+    </div>
+  )
+}
