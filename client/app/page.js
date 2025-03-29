901771890e488195b5e913a357d61a05a8df1e89
thomas-almeida <contato.thomasalmeidard@gmail.com>
2025-03-29 16:32:33 -0300

    Login page settings

--- client/app/page.js
+++ client/app/page.js
@@ -1,9 +1,23 @@
+'use client'
+
 import CardBoxComposition from "@/components/compositions/CardBoxComposition";
 import SignSettings from "@/components/settings/SignSettings";
 import InnovationInput from "@/components/ui/InnovationInput";
 import LoginButton from "@/components/ui/LoginButton";
+import { useMemo, useState } from "react";
 
 export default function Home() {
+
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  const payload = {
+    email: username,
+    senha: password
+  }
+
+  const isDisable = useMemo(() => username.length !== 0 && password.length !== 0 ? false : true, [username, password])
+
   return (
     <main className="flex justify-center items-center h-svh">
       <div className="lg:w-[45%] border-slate-200 p-4">
@@ -15,14 +29,21 @@ export default function Home() {
             icon={'/user.png'}
             type={'text'}
             placeholder={"Usuário"}
+            value={username}
+            setValue={setUsername}
           />
           <InnovationInput
             icon={'/password.png'}
             type={'password'}
             placeholder={"Senha"}
+            value={password}
+            setValue={setPassword}
           />
           <SignSettings />
-          <LoginButton />
+          <LoginButton
+            isDisable={isDisable}
+            payload={payload}
+          />
         </CardBoxComposition>
       </div>
     </main>
