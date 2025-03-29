a0c64126b4bc1a43e5f324988f86df68260b6e65
thomas-almeida <contato.thomasalmeidard@gmail.com>
2025-03-29 16:33:56 -0300

    Login Button Settings

--- client/components/ui/LoginButton.jsx
+++ client/components/ui/LoginButton.jsx
@@ -1,10 +1,24 @@
-export default function LoginButton({
+import { redirect } from "next/navigation"
+import service from "@/service"
 
+export default function LoginButton({
+  isDisable,
+  payload
 }) {
+
+  async function SignIn() {
+    const response = await service.signIn(payload)
+    redirect(response.data.status === 1 ? '/produtos' : '')
+  }
+
   return (
     <div className="flex justify-center items-center mt-10">
       <button
-        className="bg-white rounded-full p-4 text-black font-semibold w-[45%] cursor-pointer"
+        className={
+          `${isDisable ? `bg-slate-300 text-slate-400` : `bg-white`} 
+          rounded-full p-4 text-black font-semibold w-[45%] cursor-pointer`
+        }
+        onClick={() => SignIn()}
       >
         Login
       </button>
