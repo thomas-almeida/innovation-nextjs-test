70edcfe6241d046e8e650ff0b643b7f2289fae49
thomas-almeida <contato.thomasalmeidard@gmail.com>
2025-03-29 14:55:36 -0300

    CardBox Container

---
+++ client/components/compositions/CardBoxComposition.jsx
@@ -0,0 +1,12 @@
+
+export default function CardBoxComposition({ children }) {
+  return (
+    <>
+      <div className="bg-[#80bc04] p-10 py-14 rounded-md flex justify-center items-center">
+        <div className="w-[80%] text-white">
+          {children}
+        </div>
+      </div>
+    </>
+  )
+}
