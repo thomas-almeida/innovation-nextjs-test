3a5ec177aba7cfa18d13255a23523f271b22c8fe
thomas-almeida <contato.thomasalmeidard@gmail.com>
2025-03-31 00:46:15 -0300

    Product Dynamic Page

---
+++ client/app/produtos/[productId]/page.js
@@ -0,0 +1,118 @@
+'use client'
+
+import ContainerContentComposition from '@/components/compositions/ContainerContentComposition'
+import Navbar from '@/components/ui/Navbar'
+import Pricing from '@/components/ui/Pricing'
+import ProductButton from '@/components/ui/ProductButton'
+import service from '@/service'
+import useUserStore from '@/store/userStore'
+import Image from 'next/image'
+import { use, useEffect, useState } from 'react'
+import { Reload } from 'react-ionicons'
+
+export default function ProductPage({ params }) {
+  const { productId } = use(params)
+  const { user, _hasHydrated } = useUserStore()
+  const [productData, setProductData] = useState(null)
+
+  const mockArr = [0, 1, 2, 3]
+
+  useEffect(() => {
+
+    if (_hasHydrated && user?.token) {
+      const getProductById = async () => {
+        try {
+          const response = await service.getProductById(productId, user.token)
+          setProductData(response.data[0])
+          console.log(response.data[0])
+        } catch (error) {
+          console.error("Erro ao buscar produto:", error)
+        }
+      }
+
+      getProductById()
+    }
+  }, [_hasHydrated, user?.token, productId])
+
+  if (!productData) {
+    return (
+      <div className='flex justify-center items-center h-svh'>
+        <Reload
+          width="20px"
+          heigth="20px"
+          rotate
+        />
+      </div>
+    )
+  }
+
+  return (
+    <>
+      <Navbar userData={user} />
+      <main>
+        <ContainerContentComposition>
+          <div className='py-10 flex justify-between items-center gap-6'>
+            <div>
+              <div className='flex justify-center border border-slate-100 my-2 rounded-md shadow-sm'>
+                <Image
+                  src={productData?.imagem}
+                  width={300}
+                  height={300}
+                  alt={productData.descricao}
+                />
+              </div>
+              <div className=''>
+                <div className='grid grid-cols-4 gap-4'>
+                  {
+                    mockArr.map((image) => (
+                      <div className='shadow-sm px-2 rounded-md hover:border-slate-300 cursor-pointer'>
+                        <Image
+                          key={image}
+                          src={productData?.imagem}
+                          width={120}
+                          height={100}
+                          alt={productData.descricao}
+                        />
+                      </div>
+                    ))
+                  }
+                </div>
+              </div>
+            </div>
+            <div className='w-[70%]'>
+              <div className=''>
+                <h1 className='text-2xl font-semibold'>{productData.nome}</h1>
+                <div className='my-4'>
+                  <div className='flex justify-start items-center gap-2'>
+                    <Pricing
+                      value={productData.preco}
+                      style={"font-semibold text-4xl"}
+                    />
+                    <p>unidade</p>
+                  </div>
+                  <div className='w-full'>
+                    <h4 className='flex w-[60%] justify-center items-center gap-2 px-2 border border-slate-200 rounded-full'>
+                      <p>Total (Min. 5) </p>
+                      <Pricing value={parseFloat(productData.preco * 5)} />
+                    </h4>
+                  </div>
+                </div>
+                <div className='my-2'>
+                  <h4 className='font-semibold text-xl'>Descrição do Produto</h4>
+                  <p className='py-2'>
+                    {productData.descricao}
+                  </p>
+                </div>
+                <div>
+                  <ProductButton
+                    text={"Comprar Via PIX"}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </ContainerContentComposition>
+      </main>
+    </>
+  )
+}
